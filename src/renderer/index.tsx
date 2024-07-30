bd64c8da9e2c99d9b164f84055e84a196718aafb
Spoekle <Thianjaygraber@gmail.com>
2024-07-30 02:17:26 +0200

    default stuff gone

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -12,4 +12,3 @@ window.electron.ipcRenderer.once('ipc-example', (arg) => {
   // eslint-disable-next-line no-console
   console.log(arg);
 });
-window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
