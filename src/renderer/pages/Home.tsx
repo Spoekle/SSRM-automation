ca0c18405abdb3e280035b2055126e36e9a74721
Spoekle <Thianjaygraber@gmail.com>
2024-07-30 02:17:51 +0200

    actueally added text

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 function Home() {
 
   return (
-    <div className='bg-neutral-800 text-gray-200 pt-4'>
-      <div className='items-center justify-center'>
-        <div className='text-center text-3xl'>
-          <h1>Home</h1>
-        </div>
+    <div className='grid justify-items-center dark:text-neutral-200 bg-neutral-200 dark:bg-neutral-900 p-4 pt-8 justify-center items-center'>
+      <div className='items-center justify-items-center'>
         <div className='text-center'>
-
+          <h1 className='text-2xl font-bold'>SSRM Automation Tool</h1>
+          <p className='text-lg'>Generate everything that is needed for a BeatSaber Video!</p>
+        </div>
+        <div className='items-center justify-center'>
+          
         </div>
       </div>
     </div>
