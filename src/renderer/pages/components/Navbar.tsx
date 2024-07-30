7ff92733b9519b5876ff44f71c66f6a478f75078
Spoekle <Thianjaygraber@gmail.com>
2024-07-30 02:18:15 +0200

    navbar update

--- src/renderer/pages/components/Navbar.tsx
+++ src/renderer/pages/components/Navbar.tsx
@@ -4,13 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className='bg-neutral-800 text-gray-200'>
-      <div className='items-center justify-center'>
-        <div className='text-center'>
-          <h1 className='text-2xl'>ScoreSaber Ranked Maps - Automation</h1>
-          <p className='text-lg'>Created by Spoekle</p>
-        </div>
-        <div className='text-center text-lg mt-2'>
+    <div className='h-16 items-center justify-center bg-neutral-200 dark:bg-neutral-950 text-gray-950 dark:text-gray-200 p-4'>
+      <div className='flex text-center justify-between'>
+        <h1 className='text-xl'>ScoreSaber Ranked Maps - Automation</h1>
+        <div className='text-center text-lg'>
           <Link to='/' className='text-blue-500 mr-2 hover:underline'>Home</Link>
           <Link to='/titles' className='text-blue-500 mx-2 hover:underline'>Titles</Link>
           <Link to='/thumbnails' className='text-blue-500 ml-2 hover:underline'>Thumbnails</Link>
