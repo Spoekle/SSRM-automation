3a779d015902d89539815e93b7824a940e56a7b0
Spoekle <Thianjaygraber@gmail.com>
2024-09-15 14:00:53 +0200

    alert fixes

--- src/renderer/pages/Titles.tsx
+++ src/renderer/pages/Titles.tsx
@@ -15,17 +15,18 @@ interface MapInfo {
   }[];
 }
 
-interface CopyAlert {
+interface Alert {
   id: number;
   message: string;
   fadeOut: boolean;
+  type: 'success' | 'error' | 'alert';
 }
 
 const Titles: React.FC = () => {
   const [mapId, setMapId] = useState<string>('');
   const [difficulty, setDifficulty] = useState<string>('Easy');
   const [player, setPlayer] = useState<string>('Mr_bjo');
-  const [copyAlerts, setCopyAlerts] = useState<CopyAlert[]>([]);
+  const [alerts, setAlerts] = useState<Alert[]>([]);
   const [mapInfo, setMapInfo] = useState<MapInfo | null>(null);
   const [mapFormModal, setMapFormModal] = useState<boolean>(false);
   const [useSubname, setUseSubname] = useState<boolean>(false);
@@ -41,19 +42,31 @@ const Titles: React.FC = () => {
     }
   }, []);
 
+  const removeMapInfo = () => {
+    setMapId('');
+    setMapInfo(null);
+    localStorage.removeItem('mapId');
+    localStorage.removeItem('mapInfo');
+    createAlerts('Cleared map info!', 'alert');
+  };
+
   const copyToClipboard = (text: string, type: 'title' | 'description') => {
     navigator.clipboard.writeText(text).then(() => {
-      const id = new Date().getTime();
-      setCopyAlerts([...copyAlerts, { id, message: `Copied ${type} to clipboard!`, fadeOut: false }]);
-      setTimeout(() => {
-        setCopyAlerts(copyAlerts => copyAlerts.map(alert => alert.id === id ? { ...alert, fadeOut: true } : alert));
-        setTimeout(() => {
-          setCopyAlerts(copyAlerts => copyAlerts.filter(alert => alert.id !== id));
-        }, 500);
-      }, 2000);
+      createAlerts(`$Copied ${type} to clipboard!`, 'success');
     }).catch((err) => {
       console.error('Failed to copy: ', err);
     });
+  };
+
+  const createAlerts = (text: string, type: 'success' | 'error' | 'alert') => {
+    const id = new Date().getTime();
+    setAlerts([...alerts, { id, message: `${text}`, type, fadeOut: false }]);
+    setTimeout(() => {
+      setAlerts(alerts => alerts.map(alert => alert.id === id ? { ...alert, fadeOut: true } : alert));
+      setTimeout(() => {
+        setAlerts(alerts => alerts.filter(alert => alert.id !== id));
+      }, 500);
+    }, 2000);
   };
 
   const mapLink = `https://beatsaver.com/maps/${mapId}`;
@@ -71,12 +84,20 @@ const Titles: React.FC = () => {
             Open Map Form
           </button>
           {mapInfo && (
-              <div className='absolute left-0 top-0 mt-4 ml-4 z-10 drop-shadow-lg'>
-                <div className='bg-neutral-300 dark:bg-neutral-800 mt-2 p-2 rounded-md hover:scale-110 transition duration 200'>
-                    <a href={mapLink} target="_blank"><img className='w-24 h-24 rounded-md no-move' src={mapInfo.versions[0].coverURL} alt='Cover' /></a>
-                </div>
+            <div className='absolute left-0 top-0 mt-4 ml-4 z-10 drop-shadow-lg hover:drop-shadow-lg flex'>
+              <div className='bg-neutral-300 dark:bg-neutral-800 mt-2 p-2 rounded-md hover:scale-110 transition duration-200'>
+                <a href={mapLink} target="_blank" title="Go to BeatSaver map page">
+                  <img className='w-24 h-24 rounded-t-md no-move' src={mapInfo.versions[0].coverURL} alt='Cover' />
+                </a>
+                <button
+                  className='w-24 bg-red-500 hover:bg-red-700 text-white text-sm font-bold py-2 px-4 rounded-b-md transition duration-200'
+                  onClick={() => removeMapInfo()}
+                >
+                  Clear Map Info
+                </button>
               </div>
-            )}
+            </div>
+          )}
         </div>
 
         {mapInfo && (
@@ -119,10 +140,9 @@ const Titles: React.FC = () => {
           </div>
         )}
       </div>
-      <div className='absolute top-0 right-0 mt-4 mr-4 flex flex-col items-end space-y-2'>
-        {copyAlerts.map(alert => (
-          <div key={alert.id} className={`flex items-center justify-center px-4 py-2 bg-green-600 rounded-md drop-shadow-lg animate-fade-left ${alert.fadeOut ? 'animate-fade-out' : ''}`}>
-            <FaClipboard className='text-white mr-2'/>
+      <div className='absolute top-0 right-0 mt-4 mr-4 flex flex-col items-end space-y-2 overflow-hidden z-60'>
+        {alerts.map(alert => (
+          <div key={alert.id} className={`flex items-center justify-center px-4 py-2 ${alert.type === 'success' ? 'bg-green-600' : alert.type === 'error' ? 'bg-red-600' : 'bg-blue-600'} rounded-md drop-shadow-lg animate-fade-left ${alert.fadeOut ? 'animate-fade-out' : ''}`}>
             <p className='text-white'>{alert.message}</p>
           </div>
         ))}
