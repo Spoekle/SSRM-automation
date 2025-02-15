c06c5c36783e2b7f73233060f0740fb2dee02a00
Spoekle <Thianjaygraber@gmail.com>
2025-02-15 22:39:09 +0100

    fixed modal closing when selecting text and letting the mouse go outside of the modal

--- src/renderer/pages/Cards/components/CardForm.tsx
+++ src/renderer/pages/Cards/components/CardForm.tsx
@@ -277,7 +277,7 @@ const CardForm: React.FC<CardFormProps> = ({
   return ReactDOM.createPortal(
     <div
       className="modal-overlay fixed inset-0 bg-black/20 dark:bg-white/10 backdrop-blur-lg flex justify-center items-center z-50 rounded-3xl animate-fade animate-duration-200"
-      onClick={handleClickOutside}
+      onMouseDown={handleClickOutside}
     >
       <div className="relative modal-content bg-neutral-200 dark:bg-neutral-900 text-neutral-950 dark:text-neutral-200 p-6 m-16 rounded-lg animate-jump-in animate-duration-300">
         <div className='absolute top-8 right-8 text-center items-center text-lg'>
